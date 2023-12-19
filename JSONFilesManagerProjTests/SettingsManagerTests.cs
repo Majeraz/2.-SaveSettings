59e5f37bf4a946d75994e2517f761e7eb1e03060
Norbert <wrotkarz.95@gmail.com>
2023-12-19 12:32:16 +0100

    PDF Creation created

--- JSONFilesManagerProjTests/SettingsManagerTests.cs
+++ JSONFilesManagerProjTests/SettingsManagerTests.cs
@@ -29,47 +29,4 @@ public class MyProject {
 public class SettingsManagerTests {
     public SettingsManagerTests() {
     }
-
-    [TestMethod()]
-    public void ConvertJObjectTypeToTheTypeIfOccuredTest_ObjectIsString() {
-        string testString = "test";
-        SettingsManager<int> settingsManager = new();
-        settingsManager.ConvertJObjectTypeToTheTypeIfOccured(testString);
-        Assert.IsTrue(testString == "test");
-    }
-    [TestMethod()]
-    public void ConvertJObjectTypeToTheTypeIfOccuredTest_ObjectIsListOfStrings() {
-        List<string> testStringList = new() { "test1", "test1" };
-        List<string> originalList = new() { "test1", "test1" };
-        SettingsManager<int> settingsManager = new();
-        settingsManager.ConvertJObjectTypeToTheTypeIfOccured(testStringList);
-        bool notTrueHelperBool = false;
-        for (int i = 0; i < testStringList.Count; i++) {
-            if (testStringList[i] != originalList[i])
-                notTrueHelperBool = true;
-        }
-        Assert.IsFalse(notTrueHelperBool);
-    }
-    [TestMethod()]
-    public void ConvertJObjectTypeToTheTypeIfOccuredTest_ObjectIsListOfClassAtSpecifiedType() {
-        MyProject myProject = new();    // Create new project
-        MySubassembly mySubassembly = new();    // Create new Subassembly
-        List<MySubassembly> mySubassemblyList = new() { mySubassembly };    // Create mySubassemblyList
-        string componentString = "componentStringValue";    // Create a string member for mySubassemblyList[0].components
-        MySubassembly mySubassemblySubassembliedInComponents = new();   // Create a MySubassembly member for mySubassemblyList[0].components
-        mySubassemblyList[0].components = new() { componentString, mySubassemblySubassembliedInComponents };    // Add those members to mySubassemblyList[0].components
-        JObject jObject = new() { };
-        mySubassemblySubassembliedInComponents.components.Add(jObject); // 
-
-        List<string> testStringList = new() { "test1", "test1" };
-        List<string> originalList = new() { "test1", "test1" };
-        SettingsManager<int> settingsManager = new();
-        settingsManager.ConvertJObjectTypeToTheTypeIfOccured(testStringList);
-        bool notTrueHelperBool = false;
-        for (int i = 0; i < testStringList.Count; i++) {
-            if (testStringList[i] != originalList[i])
-                notTrueHelperBool = true;
-        }
-        Assert.IsFalse(notTrueHelperBool);
-    }
 }
