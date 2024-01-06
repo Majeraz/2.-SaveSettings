89fe25f66aa8339be90aae6e2c863fc54897ae06
Norbert <wrotkarz.95@gmail.com>
2024-01-06 14:41:53 +0100

    Clear instruction added, code's been just refactored

--- SaveSettingsApp/Program.cs
+++ SaveSettingsApp/Program.cs
@@ -3,18 +3,16 @@
 
 
 // Files examples 
-string JSONFileRelativeDirectoryPath= "Settings\\";
-string JSONFileRelativePath = JSONFileRelativeDirectoryPath + "testSetting.json";
+string JSONFileRelativeDirectoryPath= "Settings\\"; // Define a relative directory of the *.json file
+string JSONFileRelativePath = JSONFileRelativeDirectoryPath + "testSetting.json";   // Define a name for the file
 
 // DEBUG TO SEE HOW IT WORKS
 // Code usage example for manipulate JSON files
 
-List<string> myStringList = new() {
-	"test1",
-	"test2",
-};
+List<string> myStringList = new();
 
 
-//SettingsManager<List<string>> settingManager = new(JSONFileRelativePath, myStringList);
-//settingManager.AddObjectToJSONFile(myStringList);
-//settingManager.GetSettings();
+SettingsManager<List<string>> settingManager = new(JSONFileRelativePath, ref myStringList); // Pass object to be supported by settings manager
+myStringList.Add("test");   // Modify the object part 1/2
+myStringList.Add("test2");   // Modify the object part 2/2
+settingManager.RewriteSetting();    // Rewrite settings
