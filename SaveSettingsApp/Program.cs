6df20583ca6f7da1436e3a43599f2b047620cd10
Norbert <wrotkarz.95@gmail.com>
2024-04-24 08:59:07 +0200

    .NET 6 to .NET8 switch + projects' types changed on class library

--- SaveSettingsApp/Program.cs
+++ SaveSettingsApp/Program.cs
@@ -2,17 +2,17 @@
 
 
 
-// Files examples 
-string JSONFileRelativeDirectoryPath = "Settings\\"; // Define a relative directory of the *.json file
-string JSONFileRelativePath = JSONFileRelativeDirectoryPath + "testSetting.json";   // Define a name for the file
+//// Files examples 
+//string JSONFileRelativeDirectoryPath = "Settings\\"; // Define a relative directory of the *.json file
+//string JSONFileRelativePath = JSONFileRelativeDirectoryPath + "testSetting.json";   // Define a name for the file
 
-// DEBUG TO SEE HOW IT WORKS
-// Code usage example for manipulate JSON files
+//// DEBUG TO SEE HOW IT WORKS
+//// Code usage example for manipulate JSON files
 
-List<string> myStringList = new();
+//List<string> myStringList = new();
 
 
-SettingsManager<List<string>> settingManager = new(JSONFileRelativePath, ref myStringList); // Pass object to be supported by settings manager
-myStringList.Add("test");   // Modify the object part 1/2
-myStringList.Add("test2");   // Modify the object part 2/2
-settingManager.RewriteSetting();    // Rewrite settings
+//SettingsManager<List<string>> settingManager = new(JSONFileRelativePath, ref myStringList); // Pass object to be supported by settings manager
+//myStringList.Add("test");   // Modify the object part 1/2
+//myStringList.Add("test2");   // Modify the object part 2/2
+//settingManager.RewriteSetting();    // Rewrite settings
