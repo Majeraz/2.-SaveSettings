cf261c4b7370458724a19b1b3e5f178a1e00b4b8
Norbert <wrotkarz.95@gmail.com>
2024-05-04 14:24:01 +0200

    Small refactoring

---
+++ SaveSettingsApp/Instruction.cs
@@ -0,0 +1,22 @@
+﻿using JSONFilesManagerProj;
+
+
+internal class Instruction {
+    public Instruction()
+    {
+        // Files examples 
+        string JSONFileRelativeDirectoryPath = "Settings\\"; // Define a relative directory of the *.json file
+        string JSONFileRelativePath = JSONFileRelativeDirectoryPath + "testSetting.json";   // Define a name for the file
+
+        // DEBUG TO SEE HOW IT WORKS
+        // Code usage example for manipulate JSON files
+
+        List<string> myStringList = new();
+
+
+        SettingsManager<List<string>> settingManager = new(JSONFileRelativePath, ref myStringList); // Pass object to be supported by settings manager
+        myStringList.Add("test");   // Modify the object part 1/2
+        myStringList.Add("test2");   // Modify the object part 2/2
+        settingManager.RewriteSetting();    // Rewrite settings
+    }
+}
